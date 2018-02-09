466a05804ddd6f9e89338fd23e7a93f2236428ed
Amanda Cavallaro <amandaresolver@Amandas-MacBook-Pro.local>
2018-02-09 00:35:06 +0000

    adds hamster

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ class App extends Component {
     super(props);
 
     this.catImage = "https://static.pexels.com/photos/20787/pexels-photo.jpg";
-    this.imageCat = `<img height='150' width='200' src=${this.catImage} />`;
+    this.imageCat = `<img alt="cat" height='150' width='200' src=${this.catImage} />`;
     this.dogImage = "https://i.pinimg.com/originals/ec/3a/aa/ec3aaaa969157aa5fff993c5f76abee4.jpg";
-    this.imageDog = `<img height='50' width='50' src=${this.dogImage} />`;
+    this.imageDog = `<img alt="dog" height='150' width='200' src=${this.dogImage} />`
+    this.noAnimal = `<img height='150' width='200' />`
+    this.hamsterImage = "https://www.unilad.co.uk/wp-content/uploads/2017/07/hamster-hehe.jpg";
+    this.imageHamster = `<img alt="dog" height='150' width='200' src=${this.hamsterImage} />`;
+
     this.filter = ['contrast(4)', 'brightness(3)', 'blur(5px)', 'sepia(50%)'];
+    
     this.showHTMLOutput = this.showHTMLOutput.bind(this);
     this.handleWebCam = this.handleWebCam.bind(this);
     this.handleCat = this.handleCat.bind(this);
@@ -23,16 +28,14 @@ class App extends Component {
       on: false,
       image: false,
       showWebcam: true,
-      animal: this.imageCat,
+      animal: this.noAnimal,
       style: "none"
     };
-
-
   }
 
-  onChange(newValue, e) {
-    console.log('onChange', newValue, e);
-  }
+  // onChange(newValue, e) {
+  //   console.log('onChange', newValue, e);
+  // }
 
   handleWebCam(e) {
     this.setState({
@@ -49,19 +52,20 @@ class App extends Component {
   handleCat = () => {
     this.setState({ screenshot: this.catImage });
     this.setState({ image: !this.state.image });
+    this.setState({ animal: this.imageCat });
   }
+  
 
   handleDog = () => {
     this.setState({ screenshot: this.dogImage });
     this.setState({ image: !this.state.image });
-  }
-
-  handleAnimalDog = () => {
     this.setState({ animal: this.imageDog });
   }
 
-  handleAnimalCat = () => {
-    this.setState({ animal: this.imageCat });
+  handleHamster = () => {
+    this.setState({ screenshot: this.hamsterImage });
+    this.setState({ image: !this.state.image });
+    this.setState({ animal: this.imageHamster});
   }
 
   showHTMLOutput() {
@@ -83,16 +87,16 @@ class App extends Component {
   }
 
   render() {
-    
+
     const numbers = [0, 1, 2, 3];
     const listItems = numbers.map((number) =>
       <span key={number.toString()}>{this.state.image ?
-        <img height="150" onClick={() => this.handleStyle(number)} width="200" id='CSSFilterscreenshotTaken1' alt='animal'  style={{ filter: this.filter[number], WebkitFilter: this.filter[number] }} src={this.state.screenshot}  />
+        <img height="150" onClick={() => this.handleStyle(number)} width="200" id='CSSFilterscreenshotTaken1' alt='animal' style={{ filter: this.filter[number], WebkitFilter: this.filter[number] }} src={this.state.screenshot} />
         : null}
       </span>
     );
     let textareaValue = "<h1 style='color:red;'> Hi, my name is [replace with your name]. </h1>\n<h2 style='color:blue;'> I am practicing my coding skills. </h2>\n<h3 style='color:green;'> Cute pet below: </h3>\n" + this.state.animal;
-    
+
     return (
       <div className="App" id="App">
         <div className="container-fluid">
@@ -103,7 +107,7 @@ class App extends Component {
             <div className="col-sm-6" id="input">
               <h3>Type HTML code here</h3>
               <div >
-                <textarea id="HTMLInserted" defaultValue={textareaValue} onChange={this.showHTMLOutput} />
+                <textarea id="HTMLInserted"  ref="text" defaultValue={textareaValue} onChange={this.showHTMLOutput} />
               </div>
             </div>
             <div className="col-sm-6" >
@@ -111,44 +115,28 @@ class App extends Component {
               <div id="result">
                 <div id="output"></div>
               </div>
-              <div id="submit">
-                {/* save it somewhere */}
-                <button className="btn btn-danger btn-lg">Submit amazing work!</button>
-              </div>
+              {/* <div id="submit">
+                save it somewhere
+                <button className="btn btn-danger btn-lg">Submit amazing work!</button> */}
+              {/* </div> */}
             </div>
-
-            <div >
-              <div id="image" className="code_box">
-                <div className="col-sm-12">
-                  <h3>Click here to show image filters</h3>
-                 
-                  <div>
-                    <img alt="cat" onClick={this.handleCat} height='150' width='200' src={this.catImage} />
-                    <img alt="dog" onClick={this.handleDog} height='150' width='200' src={this.dogImage} />
-                  </div>
-                </div>
+            <div className="col-md-6" id="image">
+              <h3>Click here to show image filters</h3>
+              <div>
+                <img alt="cat" onClick={this.handleCat} height='150' width='200' src={this.catImage} />
+                <img alt="dog" onClick={this.handleDog} height='150' width='200' src={this.dogImage} />
+                <img alt="dog" onClick={this.handleHamster} height='150' width='200' src={this.hamsterImage} />
               </div>
             </div>
             <div className="col-sm-6">
               <div className='screenshots'>
-
-               {/* <button type="button" className={this.state.showWebcam ? "btn btn-danger" : "btn btn-success"} onClick={this.handleWebCam}>
-                    {
-                      this.state.showWebcam ? (
-                        <span > Choose filter</span>
-                      ) :
-                        <span > Hide </span>
-                    }
-                  </button> */}
                 {this.state.screenshot ?
                   <div>
-                    <div>
-                      <h2>Click to see the css style of filter below</h2>
-                      <div className='webcams'>
-                        {listItems}
-                      </div>
+                    <h3>Click to see the css style of filter below</h3>
+                    {listItems}
+                    <div style={{ fontSize: '26px' }}>
+                      Copy style below: <span style={{ fontFamily: 'COURIER' }} >style="{this.state.style}" </span>
                     </div>
-                    <h1> Copy style below: style="{this.state.style}" </h1>
                   </div>
                   : null}
               </div>
