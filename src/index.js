10d76817409f478fcd3acbde6c52ea2c1b33de25
Amanda Cavallaro <amandaresolver@Amandas-MacBook-Pro.local>
2018-02-09 14:28:36 +0000

    Google analytics can you please be added?

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+ReactGA.initialize('UA-113868571-1'); //Unique Google Analytics tracking number
+ReactGA.pageview(window.location.pathname);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
