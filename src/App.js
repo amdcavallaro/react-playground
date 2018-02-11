3da9deb79a5c774c7b17b96c4d5c0caf670bae28
Amanda Cavallaro <amandaresolver@Amandas-MacBook-Pro.local>
2018-02-11 10:33:42 +0000

    fixes css and missing quotation marks

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.catImage = "https://static.pexels.com/photos/20787/pexels-photo.jpg";
-    this.imageCat = `<img alt="cat" height='150' width='200' src=${this.catImage} />`;
+    this.imageCat = `<img alt="cat" height='150' width='200' src='${this.catImage}' />`;
     this.dogImage = "https://i.pinimg.com/originals/ec/3a/aa/ec3aaaa969157aa5fff993c5f76abee4.jpg";
-    this.imageDog = `<img alt="dog" height='150' width='200' src=${this.dogImage} />`
+    this.imageDog = `<img alt="dog" height='150' width='200' src='${this.dogImage}' />`
     this.noAnimal = `<img height='150' width='200'  />`
     this.hamsterImage = "https://www.unilad.co.uk/wp-content/uploads/2017/07/hamster-hehe.jpg";
-    this.imageHamster = `<img alt="hamster" height='150' width='200' src=${this.hamsterImage} />`;
+    this.imageHamster = `<img alt="hamster" height='150' width='200' src='${this.hamsterImage}' />`;
 
 
     this.filter = ['contrast(4)', 'brightness(3)', 'blur(5px)', 'sepia(50%)'];
@@ -26,8 +26,8 @@ class App extends Component {
     this.handleStyle = this.handleStyle.bind(this);
     this.handleTextareaChange = this.handleTextareaChange.bind(this);
     let defaultTextareaContent = `
-    <h1 style='color:red;'> Hi, my name is [replace with your name].
-    <h2 style='color:blue;'> I am practicing my coding skills.</h2>
+    <h1 style='color:red;'> Hi, my name is [replace with your name]. </h1>
+    <h2 style='color:blue;'> I am practicing my coding skills. </h2>
     <h3 style='color:green;'> Cute pet below: </h3>
     ${this.noAnimal}`;
     this.state = {
@@ -97,12 +97,11 @@ class App extends Component {
               imageHamster={this.imageHamster}
               handleCat={this.handleCat}
               handleDog={this.handleDog}
-              handleHamster={this.handleHamster}
-            />
-            <FilterComponent 
-            screenshot={this.state.screenshot}
-            style={this.state.style}
-            handleStyle={this.handleStyle} />
+              handleHamster={this.handleHamster}/>
+            <FilterComponent
+              screenshot={this.state.screenshot}
+              style={this.state.style}
+              handleStyle={this.handleStyle} />
           </div>
         </div>
       </div>
